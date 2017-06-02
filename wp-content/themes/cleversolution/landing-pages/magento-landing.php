d7ff034c1e7aa0062aa681921716794440014d49
gerasimchuk4clever-solution <gerasimchuk@clever-solution.com>
2017-06-02 18:08:06 +0300

    layout magento landing(desktop finish)

---
+++ wp-content/themes/cleversolution/landing-pages/magento-landing.php
@@ -0,0 +1,341 @@
+<?php
+/**
+ *Template Name: Magento landing
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that other
+ * 'pages' on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Clever_Solution
+ * @since Clever Solution 1.0
+ */
+get_header(); ?>
+
+<div class="magentoLp">
+    <section class="magentoLp-top">
+        <div class="leftBlock">
+                <div class="title">
+                    <a href="/home"><img src="<?php echo get_template_directory_uri()?>/images/logo-white.png" alt=""></a>
+                    <p>Get Started in the eCommerce Industry Today</p>
+                    <h2>With Our Outstanding Magento Solutions</h2>
+                </div>
+        </div>
+        <div class="rightBlock">
+            <img src="<?php echo get_template_directory_uri()?>/images/landing-pages/magento-landing/bg-top-right.png" alt="">
+            <p class="txt">Start Now</p>
+        </div>
+    </section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 nopadding-mob">
+                <div class="form-landing">
+                    <div class="container">
+                        <div class="forms">
+                            <?php echo do_shortcode('[contact-form-7 id="711" title="SEO LANDING FORM" html_name="Landing Form"]'); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="magento-about">
+        <div class="container">
+            <div class="box col-md-6 col-md-offset-3">
+                <div class="col-md-3 center">
+                    <img src="<?php echo get_template_directory_uri()?>/images/landing-pages/magento-landing/magento-about-logo.png" alt="">
+                </div>
+                <div class="col-md-9">
+                    <p><strong>Magento</strong> – a content management system (CMS) for ecommerce websites. It is the most commonly used and most popular platform in the world for building flexible, easily scalable and competitive online stores. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="magento-more">
+        <img src="<?php echo get_template_directory_uri()?>/images/landing-pages/magento-landing/magento-more.png" alt="">
+        <h2>More than 250k merchants</h2>
+        <p>across the globe sell using Magento</p>
+    </section>
+    <section class="magento-txt">
+        <div class="container">
+            <div class="box col-md-6 col-md-offset-3">
+                <div class="title">Forget about</div>
+                <p class="desc">TIME-CONSUMING <br> CUSTOM PHP & CUSTOM CMS <br> DEVELOPMENT</p>
+            </div>
+            <div class="col-md-6 col-md-offset-3">
+                <div class="title">No need to</div>
+                <p class="desc">REINVENT THE WHEEL</p>
+            </div>
+        </div>
+    </section>
+    <section class="magento-desc">
+        <div class="title"><span>Magento is the best option for <br> any type of ecommerce website</span></div>
+        <div class="list">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 box">
+                        <span>B2B</span>
+                        <p>SITES</p>
+                    </div>
+                    <div class="col-md-4 box">
+                        <span>B2c</span>
+                        <p>SITES</p>
+                    </div>
+                    <div class="col-md-4 box">
+                        <span>c2c</span>
+                        <p>SITES</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="animate-img">
+            <div class="container">
+                <img class="keyboard" src="<?php echo get_template_directory_uri()?>/images/landing-pages/magento-landing/keyboard.png" alt="">
+                <img class="mouse" src="<?php echo get_template_directory_uri()?>/images/landing-pages/magento-landing/mouse.png" alt="">
+            </div>
+        </div>
+        <div class="magento-informBlock">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-2">
+                        <img class="mouse" src="<?php echo get_template_directory_uri()?>/images/landing-pages/magento-landing/icon-inform.png" alt="">
+                    </div>
+                    <div class="col-md-10">
+                        <p class="txt">1 out of 4 businesses choose Magento <br> as their ecommerce platform</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="why-magento">
+        <div class="title">Why choose Magento?</div>
+        <div class="container">
+            <div class="row">
+                <ul>
+                    <li class="clearfix">
+                        <div class="col-md-2">
+                            <img class="mouse" src="<?php echo get_template_directory_uri()?>/images/landing-pages/magento-landing/why-magento1.png" alt="">
+                        </div>
+                        <div class="col-md-10">
+                            <div class="desc">
+                                <span>User-friendly content management interface</span>
+                                <p>allows shop owners to easily manage their stores</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="clearfix">
+                        <div class="col-md-2">
+                            <img class="mouse" src="<?php echo get_template_directory_uri()?>/images/landing-pages/magento-landing/why-magento2.png" alt="">
+                        </div>
+                        <div class="col-md-10">
+                            <div class="desc">
+                                <span>Mobile-friendly configuration</span>
+                                <p>makes your shop look and work properly on any device</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="clearfix">
+                        <div class="col-md-2">
+                            <img class="mouse" src="<?php echo get_template_directory_uri()?>/images/landing-pages/magento-landing/why-magento3.png" alt="">
+                        </div>
+                        <div class="col-md-10">
+                            <div class="desc">
+                                <span>Build-in and custom SEO extensions</span>
+                                <p>will contribute to your store’s stellar SEO</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="clearfix">
+                        <div class="col-md-2">
+                            <img class="mouse" src="<?php echo get_template_directory_uri()?>/images/landing-pages/magento-landing/why-magento4.png" alt="">
+                        </div>
+                        <div class="col-md-10">
+                            <div class="desc">
+                                <span>Lots of space for future growth</span>
+                                <p>thanks to its size and complexity</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="clearfix" >
+                        <div class="col-md-2">
+                            <img class="mouse" src="<?php echo get_template_directory_uri()?>/images/landing-pages/magento-landing/why-magento5.png" alt="">
+                        </div>
+                        <div class="col-md-10">
+                            <div class="desc">
+                                <span>100% customizable platform</span>
+                                <p>can be easily fitted to your expectations</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="clearfix">
+                        <div class="col-md-2">
+                            <img class="mouse" src="<?php echo get_template_directory_uri()?>/images/landing-pages/magento-landing/why-magento6.png" alt="">
+                        </div>
+                        <div class="col-md-10">
+                            <div class="desc">
+                                <span>Easy third-party integration</span>
+                                <p>can connect your website with multiple solutions</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="clearfix">
+                        <div class="col-md-2">
+                            <img class="mouse" src="<?php echo get_template_directory_uri()?>/images/landing-pages/magento-landing/why-magento7.png" alt="">
+                        </div>
+                        <div class="col-md-10">
+                            <div class="desc">
+                                <span>High functionality</span>
+                                <p>is ensured by a great variety of extensions and plugins</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="clearfix">
+                        <div class="col-md-2">
+                            <img class="mouse" src="<?php echo get_template_directory_uri()?>/images/landing-pages/magento-landing/why-magento8.png" alt="">
+                        </div>
+                        <div class="col-md-10">
+                            <div class="desc">
+                                <span>Safe system</span>
+                                <p>that is constantly supported by a vast number of developers</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section class="magento-services">
+        <div class="container">
+            <div class="title">Our Magento Services</div>
+            <div class="row">
+                <div class="col-sm-3 center box">
+                    <div class="img">
+                        <img src="<?php echo get_template_directory_uri()?>/images/landing-pages/joomla-landing/service1.png" alt="">
+                    </div>
+                    <span class="name">Website Development</span>
+                </div>
+                <div class="col-sm-3 center box">
+                    <div class="img">
+                        <img src="<?php echo get_template_directory_uri()?>/images/landing-pages/joomla-landing/service2.png" alt="">
+                    </div>
+                    <span class="name">Website Setup</span>
+                </div>
+                <div class="col-sm-3 center box">
+                    <div class="img">
+                        <img src="<?php echo get_template_directory_uri()?>/images/landing-pages/joomla-landing/service3.png" alt="">
+                    </div>
+                    <span class="name">Themes Custom Design</span>
+                </div>
+                <div class="col-sm-3 center box">
+                    <div class="img">
+                        <img src="<?php echo get_template_directory_uri()?>/images/landing-pages/magento-landing/comment-icon.png" alt="">
+                    </div>
+                    <span class="name">Website Support</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3 center box">
+                    <div class="img">
+                        <img src="<?php echo get_template_directory_uri()?>/images/landing-pages/joomla-landing/service5.png" alt="">
+                    </div>
+                    <span class="name">Plugin Development</span>
+                </div>
+                <div class="col-sm-3 center box">
+                    <div class="img">
+                        <img src="<?php echo get_template_directory_uri()?>/images/landing-pages/joomla-landing/service6.png" alt="">
+                    </div>
+                    <span class="name">Search Engine Optimization</span>
+                </div>
+                <div class="col-sm-3 center box">
+                    <div class="img">
+                        <img src="<?php echo get_template_directory_uri()?>/images/landing-pages/joomla-landing/service7.png" alt="">
+                    </div>
+                    <span class="name">Hosting & SSL Certificate for Your Website</span>
+                </div>
+                <div class="col-sm-3 center box">
+                    <div class="img">
+                        <img src="<?php echo get_template_directory_uri()?>/images/landing-pages/joomla-landing/service8.png" alt="">
+                    </div>
+                    <span class="name">Third-Party Plugins Integration</span>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="note">
+        <div class="container">
+            <p>We know that a default Magento installation is rarely enough.
+                That’s why our team is always ready to provide a full cycle
+                of turnkey Magento development and design solutions</p>
+        </div>
+    </div>
+    <div class="magento-contactForm">
+        <div class="title">CONTACT OUR TEAM</div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 nopadding-mob">
+                    <div class="form-landing">
+                        <div class="container">
+                            <div class="forms">
+                                <?php echo do_shortcode('[contact-form-7 id="711" title="SEO LANDING FORM" html_name="Landing Form"]'); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="who-we-are">
+        <div class="title">Who We Are</div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <p class="txt"><strong>Clever Solution</strong> – is a web design studio located in NYC. Our team of experts holds Google and Bing certifications. Over the last decade, we have gathered valuable experience in web design, development and digital marketing. Clever Solution guarantees the best quality services at reasonable prices.  </p>
+                </div>
+            </div>
+        </div>
+        <img src="<?php echo get_template_directory_uri()?>/images/landing-pages/crm-development/logo-orange.png" alt="">
+    </div>
+    <div class="magento-whyClever">
+        <div class="container">
+            <div class="title"> Why Clever Solution</div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <span class="name">Our team is experienced</span>
+                </div>
+                <div class="col-sm-4">
+                    <span class="name">Our pros are dedicated</span>
+                </div>
+                <div class="col-sm-4">
+                    <span class="name">We are result-oriented</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <span class="name">We keep ourselves accountable</span>
+                </div>
+                <div class="col-sm-4">
+                    <span class="name">We respect our clients</span>
+                </div>
+                <div class="col-sm-4">
+                    <span class="name">We are ready to help 24/7</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <?php include_news() ?>
+</div>
+
+<!-- // <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script> -->
+<script type="text/javascript" src="/wp-content/themes/cleversolution/slick/slick.min.js"></script>
+<script type="text/javascript" src="/wp-content/themes/cleversolution/js/bootstrap.min.js"></script>
+<link rel='stylesheet' href='/wp-content/themes/cleversolution/css/bootstrap.min.css?ver=1.2.0' type='text/css' media='all' />
+<link rel='stylesheet' href='/wp-content/themes/cleversolution/slick/slick-theme.css' type='text/css' media='all' />
+<link rel='stylesheet' href='/wp-content/themes/cleversolution/slick/slick.css' type='text/css' media='all' />
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,300' type='text/css' media='all' />
+<link rel='stylesheet' href='/wp-content/themes/cleversolution/css/main.css' type='text/css' media='all' />
+<link rel='stylesheet' href='/wp-content/themes/cleversolution/css/magento-lp.css?2' type='text/css' media='all' />
+<link rel='stylesheet' href='/wp-content/themes/cleversolution/css/mobile-cl.css' type='text/css' media='all' />
+<?php //get_sidebar(); ?>
+<?php get_footer(); ?>
